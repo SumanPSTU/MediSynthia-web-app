5b9f0de9e0f713a325761fa668f9af33bcb4b5a9
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2026-02-02 14:41:46 +0600

    update auto create upload folder

--- config/categoryImage.js
+++ config/categoryImage.js
@@ -1,15 +1,24 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Directory path
+const categoryUploadDir = path.join(__dirname, "../uploads/category");
+
+// Ensure the directory exists
+if (!fs.existsSync(categoryUploadDir)) {
+  fs.mkdirSync(categoryUploadDir, { recursive: true }); // recursive ensures parent folders are created
+}
+
 // Store files in /uploads/category
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../uploads/category"));
+    cb(null, categoryUploadDir);
   },
   filename: (req, file, cb) => {
     const uniqueName = Date.now() + "-" + file.originalname;
@@ -26,4 +35,3 @@ const fileFilter = (req, file, cb) => {
 };
 
 export const uploadCategoryImage = multer({ storage, fileFilter });
-
